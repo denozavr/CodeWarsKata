2525eceab492585c3cdbc3f16275699e3ec212bb
Den <denizdeniz90@gmail.com>
2017-09-26 11:56:46 +0300

    1st Kata Mr. Freeze

---
+++ JS/kyu8/kata001.js
@@ -0,0 +1,12 @@
+
+/*001
+Description: There is an object/class already created called MrFreeze. 
+Mark this object as frozen so that no other changes can be made to it.
+*/
+
+//My solution
+    // mark the MrFreeze object instance as frozen
+    Object.freeze(MrFreeze);
+
+//Solution(s) I like(links): http://bit.ly/2fvP5ji 
+
