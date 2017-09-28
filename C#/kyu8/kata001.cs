7d5ec195c844f1926b54f7d2938ec0648dfc20e9
Den <denizdeniz90@gmail.com>
2017-09-28 20:44:17 +0300

    new C# kata

--- C#/kyu8/kata001.cs
+++ C#/kyu8/kata001.cs
@@ -72,3 +72,31 @@ For example:
     public static int SquareSum(int[] n) => n.Sum(i => i * i); // http://bit.ly/2xuqHG5
     public static int SquareSum(int[] n) => n.Select(x => x * x).Sum(o => o); // http://bit.ly/2xtpyhN
 
+
+
+/*003 How many lightsabers do you own?
+Description:
+Inspired by the development team at Vooza, write the function howManyLightsabersDoYouOwn that
+
+accepts the name of a programmer, and
+returns the number of lightsabers owned by that person.
+The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+
+No starting help here -- you'll need to know how to write a function that accepts a parameter and returns a value based on that parameter.
+
+    howManyLightsabersDoYouOwn("anyone else") \\ should === 0
+    howManyLightsabersDoYouOwn("Zach") \\ should === 18
+ */
+
+
+//My solution
+    public static int HowManyLightsabersDoYouOwn(string name)
+    {
+        var sabers = 0;
+        if(name == "Zach") sabers = 18;
+        return sabers;
+    }
+
+//Solution(s) I liked:
+    public static int HowManyLightsabersDoYouOwn(string name) => name == "Zach" ? 18 : 0;  // http://bit.ly/2xMX0iV
+
