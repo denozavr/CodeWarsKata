fa66b497ddd77f0dce20f62dee629ed1ca1f095b
Den <denizdeniz90@gmail.com>
2017-09-29 23:06:22 +0300

    Add Js kata

--- JS/kyu8/kata001.js
+++ JS/kyu8/kata001.js
@@ -10,3 +10,21 @@ Mark this object as frozen so that no other changes can be made to it.
 
 //Solution(s) I like(links): http://bit.ly/2fvP5ji 
 
+
+/*002 Get Planet Name By ID
+Description:
+
+The function is not returning the correct values. Can you figure out why?
+    getPlanetName(3); // should return 'Earth'
+*/
+//My Solution
+ //Same as C# 
+
+
+ //Solution(s) I like(links): 
+ function getPlanetName(i){ //http://bit.ly/2ybOFX0
+    return (['Mercury','Venus','Earth','Mars','Jupiter','Saturn','Uranus','Neptune'])[i-1];
+  } 
+
+//1) http://bit.ly/2fUmrFA
+//2) http://bit.ly/2hA2vM7 
