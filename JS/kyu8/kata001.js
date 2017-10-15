592b7315333b6859ef07ada124d455b429b9eb5b
Den <denizdeniz90@gmail.com>
2017-10-15 20:18:03 +0300

    add 8kuy JS kata

--- JS/kyu8/kata001.js
+++ JS/kyu8/kata001.js
@@ -51,3 +51,46 @@ The function is not returning the correct values. Can you figure out why?
 
 //1) http://bit.ly/2fUmrFA
 //2) http://bit.ly/2hA2vM7 
+
+
+/*003 Square(n) Sum  https://www.codewars.com/kata/square-n-sum/train/javascript
+Description:
+Complete the squareSum/square_sum/SquareSum method so that it squares each number passed into it and then sums the results together.
+
+For example:
+    squareSum([1, 2, 2]); // should return 9
+ */
+
+ //My solution
+    function squareSum(numbers){
+        var result = numbers.reduce((result, val) => result += val * val, 0);
+        return result;
+    }
+
+
+//Solutions I like:
+//1) http://bit.ly/2xGQXsC
+    function squareSum(numbers){
+        return numbers.reduce(function(sum, n){
+        return (n*n) + sum;
+        }, 0)
+    }
+//2) http://bit.ly/2wYpnrv 
+    function squareSum(numbers){
+        return numbers.map(square).reduce(sum);
+    }
+    
+    function square(number) {
+        return number * number;
+    }    
+    function sum(firstNumber, secondNumber) {
+        return firstNumber + secondNumber;
+    }
+//3) http://bit.ly/2xI89D9
+    function squareSum(numbers){
+        var sum = 0;
+        numbers.forEach(function(n) {
+            sum += n * n
+        });
+        return sum;
+    }
