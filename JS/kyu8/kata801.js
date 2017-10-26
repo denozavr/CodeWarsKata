c0156f2543e3769e7aaee8e3e214e616d6030bca
Den <denizdeniz90@gmail.com>
2017-10-26 14:03:44 +0300

    add 8kuy JS kata 'Shifty Closures'

--- JS/kyu8/kata801.js
+++ JS/kyu8/kata801.js
@@ -140,3 +140,49 @@ For example:
         var long = b.length >=a.length  ? b :a;
         return short  + long + short;
     }
+
+
+/*005 Shifty Closures(BUG)   https://www.codewars.com/kata/shifty-closures/javascript
+Description:
+Functional closures can get overly attached. Set them straight!
+Why doesn't greet_abe() actually greet Abe?
+ */
+//Intial code
+    var name = 'Abe';
+    var greet_abe = function() {
+        return "Hello, " + name + '!';
+    };
+    name = 'Ben';
+    var greet_ben = function() {
+        return "Hello, " + name + '!';
+    };
+
+  //My solution 
+    var name;
+    var greet_abe = function() {
+        name="Abe"
+        return "Hello, " + name + '!';
+    };
+    var greet_ben = function() {
+        name = 'Ben';
+        return "Hello, " + name + '!';
+    };
+
+
+//Solutions I like:
+//1) http://bit.ly/2yRQzfc
+    var greet_abe = greet('Abe');
+    var greet_ben = greet('Ben');
+
+    function greet(name) {
+        return function() {
+            return "Hello, " + name + '!';
+        };
+    }
+//2) http://bit.ly/2gDusi6 
+    const greet_abe = () => 'Hello, Abe!';
+    const greet_ben = () => 'Hello, Ben!'; 
+//3) http://bit.ly/2yNdclv
+    const greet = (name) => `Hello, ${name}!`
+    const greet_abe = () => greet('Abe')
+    const greet_ben = () => greet('Ben')   
