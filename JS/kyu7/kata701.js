2e09995e313376aa14bdf784aab564f254736bf5
Den <denizdeniz90@gmail.com>
2017-11-01 22:28:57 +0300

    add 1st 6kuy JS kata '3 and 5'(previous commit) (21st in total) + format 7kuy JS

--- JS/kyu7/kata701.js
+++ JS/kyu7/kata701.js
@@ -4,21 +4,8 @@ The following code is not giving the expected results. Can you figure out what t
 
 The following is an example of data that would be passed in to the function.
 */
-var data = [{
-    name: 'Joe',
-    age: 20
-  },
-  {
-    name: 'Bill',
-    age: 30
-  },
-  {
-    name: 'Kate',
-    age: 23
-  }
-]
+var data = [{name: 'Joe', age: 20}, {name: 'Bill', age: 30},  {name: 'Kate', age: 23}]
 getNames(data) // should return ['Joe', 'Bill', 'Kate']
-
 
 //My solution
 //add return before item.name  
