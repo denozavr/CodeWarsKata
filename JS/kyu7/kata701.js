c87703d92909d38990daa3cc0c738d4409232251
Den <denizdeniz90@gmail.com>
2018-02-21 12:30:28 +0300

    add 4th 7kuy JS kata 'Refactored Greeting' (31th in total)

--- JS/kyu7/kata701.js
+++ JS/kyu7/kata701.js
@@ -101,3 +101,46 @@ getEvenNumbers([2,4,5,6]) // should == [2,4,6]
     }
 
 //#endregion
+
+
+//#region 7004 Refactored Greeting
+/*7004 Refactored Greeting (https://www.codewars.com/kata/refactored-greeting)
+Description:
+The following code could use a bit of object oriented artistry. While its a simple method and works just fine as it is, in a larger system its best to organize methods into classes/objects.
+Refactor the following code so that it belongs to a Person class/object. Each Person instance will have a greet method. The Person instance should be instantiated with a name so that it no longer has to be passed into each greet method call.
+Here is how the final refactored code would be used:
+
+    var joe = new Person('Joe');
+    joe.greet('Kate'); // should return 'Hello Kate, my name is Joe'
+    joe.name # should == 'Joe'
+*/
+
+//My solution
+    function Person(name) {
+      this.name = name;
+
+      this.greet = function(yourName){
+        return "Hello " + yourName + ", my name is " + this.name;
+      }
+    }
+
+//Solution(s) I like(links):
+//1) Best(28) and Clever(3) https://www.codewars.com/kata/reviews/516f30297c907a79f2000662/groups/516f30297c907a79f200066a
+    function Person(name){
+      this.name = name; //added semicolons
+    }
+
+    Person.prototype.greet = function(yourName){
+      return "Hello " + yourName + ", my name is " + this.name; //added semicolons
+    }
+//2) ES6 Classes, Best(4) https://www.codewars.com/kata/reviews/516f30297c907a79f2000662/groups/55c33c790879778eda000021
+    class Person {
+      constructor(name) {
+        this.name = name;
+      }
+      greet(name) {
+        return `Hello ${name}, my name is ${this.name}`;
+      }
+    }
+
+//#endregion
