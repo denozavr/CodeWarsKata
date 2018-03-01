5cdb0f6582423afa4ba867d82d76a10663b97530
Den <denizdeniz90@gmail.com>
2018-03-01 12:11:01 +0300

    add 5th 7kuy JS kata 'Reversed Strings' (37th in total)

--- JS/kyu7/kata701.js
+++ JS/kyu7/kata701.js
@@ -144,3 +144,28 @@ Here is how the final refactored code would be used:
     }
 
 //#endregion
+
+//#region 7005 Reversed Strings
+/*7005 Reversed Strings (https://www.codewars.com/kata/reversed-strings/javascript)
+Complete the solution so that it reverses the string value passed into it.
+
+    solution('world'); // returns 'dlrow'
+*/
+
+//My solution
+    function solution(str){
+      //split string to array of chars ['h','i'], reverse and concat(join)
+      return str.split('').reverse('').join('');
+    }
+
+//Solution(s) I like(links):
+//1) Clever(17) https://www.codewars.com/kata/reviews/516f302a7c907a79f200069f/groups/586369fc248954e777000474
+    const solution = s => [...s].reverse().join('')
+//2) https://www.codewars.com/kata/reviews/516f302a7c907a79f200069f/groups/51ed435512a3da8d950000a6
+    function solution(str){ //recursion https://medium.freecodecamp.org/how-to-reverse-a-string-in-javascript-in-3-different-ways-75e4763c68cb
+      return str.length > 0 ? solution(str.substring(1)) + str.charAt(0) : '';
+    }
+//3)
+    const solution = str => str.split('').reverse().join('');
+
+//#endregion
