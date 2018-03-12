4d1da6c30d66c31637df056894e8b246d2e156d0
Den <denizdeniz90@gmail.com>
2018-03-12 17:00:26 +0300

    add 2nd 5kuy JS kata 'RGB To Hex Conversion' (47th in total)

--- JS/kyu5/kata501.js
+++ JS/kyu5/kata501.js
@@ -34,3 +34,57 @@ The following are examples of how this function would be used and what the expec
     }
 
 //#endregion
+
+
+//#region 5002 RGB To Hex Conversion
+/*5002 RGB To Hex Conversion (https://www.codewars.com/kata/rgb-to-hex-conversion)
+Description:
+  The rgb() method is incomplete. Complete the method so that passing in RGB decimal values will result in a hexadecimal representation being returned. The valid decimal values for RGB are 0 - 255. Any (r,g,b) argument values that fall out of that range should be rounded to the closest valid value.
+
+  The following are examples of expected output values:
+    rgb(255, 255, 255) // returns FFFFFF
+    rgb(255, 255, 300) // returns FFFFFF
+    rgb(0,0,0) // returns 000000
+    rgb(148, 0, 211) // returns 9400D3
+*/
+
+//My solution
+    function rgb(...rgb){ //rest parameters ES6
+      let hex = "";
+      rgb.forEach(function(entry) {
+          hex+= entry<=0 ? '00' :
+                entry>=255 ? 'FF' : entry.toString(16).toUpperCase();
+      });
+      return hex;
+    }
+
+//Solution(s) I like(links):
+//1) BEST(104) AND CLEVER(65) https://www.codewars.com/kata/reviews/516f30207c907a79f20000e6/groups/53aab4f85f1361277b000ddf
+    function rgb(r, g, b){
+      return toHex(r)+toHex(g)+toHex(b);
+    }
+
+    function toHex(d) {
+        if(d < 0 ) {return "00";}
+        if(d > 255 ) {return "FF";}
+        return  ("0"+(Number(d).toString(16))).slice(-2).toUpperCase()
+    }
+//2) BEST(22) AND CLEVER(48) https://www.codewars.com/kata/reviews/516f30207c907a79f20000e6/groups/51cc24d0d6fdd1fc3c0004ac
+    function rgb(r, g, b){
+      return [r,g,b].map(function(x) {
+        return ('0'+Math.max(0, Math.min(255, x)).toString(16)).slice(-2);
+      }).join('').toUpperCase();
+    }
+//3) Clever(55) https://www.codewars.com/kata/reviews/516f30207c907a79f20000e6/groups/51dbbf18191c27b6a1000012
+  function rgb(r, g, b) {
+    return [].slice.call(arguments).map(function (v) {
+      var ret = Math.max(0, Math.min(255, v)).toString(16).toUpperCase();
+      return (ret.length<2 ? '0' : '') + ret;
+    }).join('');
+  }
+//4) https://www.codewars.com/kata/reviews/516f30207c907a79f20000e6/groups/5aa657624eff740a770010f3
+    rgb = (r, g, b) => [r,g,b].map(el => el > 255 ? 255 : el < 0 ? 0 : el)
+                              .map(el => el.toString(16).toUpperCase())
+                              .map(el => el === "0" ? "00" : el)
+                              .join('');
+//#endregion
