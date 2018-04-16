a8aa2d46931eb1630c30dcb47ffd15ae23534921
Den <denizdeniz90@gmail.com>
2018-04-16 13:08:26 +0300

    add 6th 5kuy JS kata 'PaginationHelper' (68th in total)

--- JS/kyu5/kata501.js
+++ JS/kyu5/kata501.js
@@ -277,3 +277,98 @@ Description:
     }
 
 //#endregion
+
+
+//#region 5006 PaginationHelper
+/* 5006 PaginationHelper (https://www.codewars.com/kata/paginationhelper)
+Description:
+  For this exercise you will be strengthening your page-fu mastery. You will complete the PaginationHelper class, which is a utility class helpful for querying paging information related to an array.
+  The class is designed to take in an array of values and an integer indicating how many items will be allowed per each page. The types of values contained within the collection/array are not relevant.
+  The following are some examples of how this class is used:
+
+    var helper = new PaginationHelper(['a','b','c','d','e','f'], 4);
+    helper.pageCount(); //should == 2
+    helper.itemCount(); //should == 6
+    helper.pageItemCount(0); //should == 4
+    helper.pageItemCount(1); // last page - should == 2
+    helper.pageItemCount(2); // should == -1 since the page is invalid
+
+    // pageIndex takes an item index and returns the page that it belongs on
+    helper.pageIndex(5); //should == 1 (zero based index)
+    helper.pageIndex(2); //should == 0
+    helper.pageIndex(20); //should == -1
+    helper.pageIndex(-10); //should == -1
+*/
+
+//My solution
+    // TODO: complete this object/class
+    // The constructor takes in an array of items and a integer indicating how many
+    // items fit within a single page
+    function PaginationHelper(collection, itemsPerPage){
+      this.collection = collection
+      this.itemsPerPage = itemsPerPage
+    }
+
+    // returns the number of items within the entire collection
+    PaginationHelper.prototype.itemCount = function() {
+      return this.collection.length;
+    }
+
+    // returns the number of pages
+    PaginationHelper.prototype.pageCount = function() {
+      return Math.ceil(this.itemCount() / this.itemsPerPage);
+    }
+
+    // returns the number of items on the current page. page_index is zero based.
+    // this method should return -1 for pageIndex values that are out of range
+    PaginationHelper.prototype.pageItemCount = function(pageIndex) {
+      return this.pageCount() <= pageIndex ? -1
+           : Math.min(this.itemsPerPage, this.itemCount() - (this.itemsPerPage * pageIndex) );
+    }
+
+    // determines what page an item is on. Zero based indexes
+    // this method should return -1 for itemIndex values that are out of range
+    PaginationHelper.prototype.pageIndex = function(itemIndex) {
+      return (itemIndex < 0 || itemIndex >= this.itemCount()) ? -1
+          : Math.floor(itemIndex/this.itemsPerPage);
+
+    }
+
+
+//Solution(s) I like(links):
+//1) ES6 Best(7) https://www.codewars.com/kata/reviews/516f30257c907a79f20003e9/groups/5701319386306a876a00098a
+    class PaginationHelper {
+      constructor(collection, itemsPerPage) {
+        this.collection = collection
+        this.itemsPerPage = itemsPerPage
+        this.pages = Math.ceil(collection.length / itemsPerPage)
+      }
+
+      itemCount() {
+        return this.collection.length
+      }
+
+      pageCount() {
+        return this.pages
+      }
+
+      pageItemCount(pageIndex) {
+        if (pageIndex >= this.pages || pageIndex < 0) {
+          return -1
+        } else if (pageIndex === this.pages - 1) {
+          return (this.collection.length % this.itemsPerPage)
+        } else {
+          return this.itemsPerPage
+        }
+      }
+
+      pageIndex(itemIndex) {
+        if (itemIndex >= 0 && itemIndex < this.collection.length) {
+          return Math.max(Math.ceil(itemIndex / this.itemsPerPage) - 1, 0)
+        } else {
+          return -1
+        }
+      }
+    }
+
+//#endregion
