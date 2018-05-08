3dc2b3567550826f97617c57d9b653aa7da66e39
Den <denizdeniz90@gmail.com>
2018-05-08 13:04:32 +0300

    add 4th 4kuy JS kata 'Roman Numerals Encoder' (81st in total)

--- JS/kuy4/kata401.js
+++ JS/kuy4/kata401.js
@@ -235,3 +235,101 @@ Description:
       return rez
     }
 //#endregion
+
+
+//#region 4004 Roman Numerals Encoder
+/* 4004 Roman Numerals Encoder (https://www.codewars.com/kata/roman-numerals-encoder/javascript)
+Description:
+  Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer.
+
+  Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
+
+  Example:
+
+  solution(1000); // should return 'M'
+  Help:
+
+  Symbol    Value
+  I          1
+  V          5
+  X          10
+  L          50
+  C          100
+  D          500
+  M          1,000
+  Remember that there can't be more than 3 identical symbols in a row.
+*/
+
+//My solution
+    function solution(number){
+      var convert = {M: 1000, CM: 900, D: 500, CD: 400, C: 100, XC: 90, L: 50, XL: 40, X: 10, IX: 9, V: 5, IV: 4, I: 1};
+
+      let result = '';
+      for (let key in convert) {
+        let times = Math.floor(number / convert[key]); // how many times Roman number occur in Arabian number
+        result += key.repeat(times); // if >0 add it so many times in result
+        number -= convert[key] * times; // substract what used in result from number
+      }
+      return result;
+
+    }
+
+//Solution(s) I like(links):
+//1) BEST(23) AND CLEVER(36) https://www.codewars.com/kata/reviews/51b62bf7a9c58071c6000020/groups/53d671eb83db2702b7000c44
+    function solution(number){
+      return [
+        { threshold: 1000, char: 'M'},
+        { threshold: 900,  char: 'CM'},
+        { threshold: 500,  char: 'D'},
+        { threshold: 400,  char: 'CD'},
+        { threshold: 100,  char: 'C'},
+        { threshold: 90,   char: 'XC'},
+        { threshold: 50,   char: 'L'},
+        { threshold: 40,   char: 'XL'},
+        { threshold: 10,   char: 'X'},
+        { threshold: 9,    char: 'IX'},
+        { threshold: 5,    char: 'V'},
+        { threshold: 4,    char: 'IV'},
+        { threshold: 1,    char: 'I'}
+      ].reduce(function(prev, curr, idx, arr)
+      {
+        while(number >= curr.threshold)
+        {
+          prev += curr.char;
+          number -= curr.threshold;
+        }
+
+        return prev;
+      }, '');
+    }
+//2) Clever (153) https://www.codewars.com/kata/reviews/51b62bf7a9c58071c6000020/groups/53870848ee24bf0206001047
+    function solution(number){
+      // convert the number to a roman numeral
+    var  roman = {M:1000,CM:900, D:500,CD:400,C:100,XC:90,L:50,XL:40,X:10,IX:9,V:5,IV:4,I:1 }
+
+    var ans = '';
+    while(number>0){
+        for(a in roman){
+            if(roman[a]<=number){ ans += a; number-=roman[a]; break;}
+
+        }
+    }
+    return ans;
+    }
+//3)Clever(9) https://www.codewars.com/kata/reviews/51b62bf7a9c58071c6000020/groups/53cda73049c4ca7b5600029c
+    function solution(number)
+    {
+      var result   = '',
+          decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1],
+          roman    = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
+
+      decimals.map(function (value, index) {
+        while (number >= value) {
+          result += roman[index];
+          number -= value;
+        }
+      });
+
+      return result;
+    }
+//#endregion
