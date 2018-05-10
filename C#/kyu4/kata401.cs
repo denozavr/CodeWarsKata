06bdefe91a259f17074c974dccbefcda37a82554
Den <denizdeniz90@gmail.com>
2018-05-10 12:48:10 +0300

    add 2nd 4kuy C# kata 'Roman Numerals Encoder' (82nd in total)

--- C#/kyu4/kata401.cs
+++ C#/kyu4/kata401.cs
@@ -153,3 +153,167 @@ Description:
     }
 
 #endregion
+
+
+#region 4002 Roman Numerals Encoder
+/* 4002 Roman Numerals Encoder (https://www.codewars.com/kata/roman-numerals-encoder)
+Description:
+  Create a function taking a positive integer as its parameter and returning a string containing the Roman Numeral representation of that integer.
+
+  Modern Roman numerals are written by expressing each digit separately starting with the left most digit and skipping any digit with a value of zero. In Roman numerals 1990 is rendered: 1000=M, 900=CM, 90=XC; resulting in MCMXC. 2008 is written as 2000=MM, 8=VIII; or MMVIII. 1666 uses each Roman symbol in descending order: MDCLXVI.
+
+  Example:
+
+  RomanConvert.Solution(1000) -- should return "M"
+  Help:
+
+  Symbol    Value
+  I          1
+  V          5
+  X          10
+  L          50
+  C          100
+  D          500
+  M          1,000
+  Remember that there can't be more than 3 identical symbols in a row.
+*/
+
+//My solution
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class RomanConvert
+    {
+      public static string Solution(int n)
+      {
+        string result = "";
+        var nums = new Dictionary<string,int>()
+            {
+                { "M", 1000},
+                { "CM", 900},
+                { "D", 500},
+                { "CD", 400},
+                { "C", 100},
+                { "XC", 90},
+                { "L", 50},
+                { "XL", 40},
+                { "X", 10},
+                { "IX", 9},
+                { "V", 5},
+                { "IV", 4},
+                { "I", 1}
+            };
+        foreach (var val in nums.Keys)
+        {
+            int times = (int)Math.Floor((double)n / nums[val]); // how many times Roman number occur in Arabian number
+            result += string.Concat(Enumerable.Repeat(val, times)); // if >0 add it so many times in result
+            n -= nums[val] * times; // substract what used in result from number
+        }
+        return result;
+
+      }
+    }
+
+
+//Solution(s) I like(links):
+//1) BEST(12) AND CLEVER(35) https://www.codewars.com/kata/reviews/58a3ee9c45affb655e000375/groups/58a8efab6123a6dd40000459
+    using System;
+
+    public class RomanConvert
+    {
+      public static string Solution(int n)
+      {
+      string roman = "";
+      string[] thousand = {"","M","MM","MMM"};
+      string[] hundred = {"","C","CC","CCC","CD","D","DC","DCC","DCCC","CM"};
+      string[] ten = {"","X","XX","XXX","XL","L","LX","LXX","LXXX","XC"};
+      string[] one = {"","I","II","III","IV","V","VI","VII","VIII","IX"};
+
+      roman += thousand[(n/1000)%10];
+      roman += hundred[(n/100)%10];
+      roman += ten[(n/10)%10];
+      roman += one[n%10];
+
+      return roman;
+
+      }
+    }
+//2)Best(4) and Clever (4) https://www.codewars.com/kata/reviews/58a3ee9c45affb655e000375/groups/58dd38aae0bcd06bb800086d
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class RomanConvert
+    {
+      public static string Solution(int n)
+      {
+        var romeDict = new Dictionary<int, string>()
+        {
+            [1000] = "M", [900] = "CM", [500] = "D", [400] = "CD",
+            [100] = "C", [90] = "XC", [50] = "L", [40] = "XL",
+            [10] = "X", [9] = "IX", [5] = "V", [4] = "IV",
+            [1] = "I"
+        };
+
+        var number = new StringBuilder();
+        foreach (var romeNum in romeDict)
+        {
+            while(romeNum.Key <= n)
+            {
+                number.Append(romeNum.Value);
+                n -= romeNum.Key;
+            }
+        }
+
+        return number.ToString();
+      }
+    }
+//3) Clever(13) https://www.codewars.com/kata/reviews/58a3ee9c45affb655e000375/groups/593306ee46b5bee9430000be
+    using System;
+
+    public class RomanConvert
+    {
+      public static string Solution(int n)
+      {
+          var result = new string('I', n);
+          return result.Replace(new string('I', 1000), "M")
+                      .Replace(new string('I', 900), "CM")
+                      .Replace(new string('I', 500), "D")
+                      .Replace(new string('I', 400), "CD")
+                      .Replace(new string('I', 100), "C")
+                      .Replace(new string('I', 90), "XC")
+                      .Replace(new string('I', 50), "L")
+                      .Replace(new string('I', 40), "XL")
+                      .Replace(new string('I', 10), "X")
+                      .Replace(new string('I', 9), "IX")
+                      .Replace(new string('I', 5), "V")
+                      .Replace(new string('I', 4), "IV");
+      }
+    }
+//4) Clever(9)    https://www.codewars.com/kata/reviews/58a3ee9c45affb655e000375/groups/58a43af92ee18113d8000182
+  using System;
+
+  public class RomanConvert
+  {
+    public static string Solution(int n)
+      {
+          if ((n < 0) || (n > 3999)) throw new ArgumentOutOfRangeException("insert value betwheen 1 and 3999");
+          if (n < 1) return string.Empty;
+          if (n >= 1000) return "M" + Solution(n - 1000);
+          if (n >= 900) return "CM" + Solution(n - 900);
+          if (n >= 500) return "D" + Solution(n - 500);
+          if (n >= 400) return "CD" + Solution(n - 400);
+          if (n >= 100) return "C" + Solution(n - 100);
+          if (n >= 90) return "XC" + Solution(n - 90);
+          if (n >= 50) return "L" + Solution(n - 50);
+          if (n >= 40) return "XL" + Solution(n - 40);
+          if (n >= 10) return "X" + Solution(n - 10);
+          if (n >= 9) return "IX" + Solution(n - 9);
+          if (n >= 5) return "V" + Solution(n - 5);
+          if (n >= 4) return "IV" + Solution(n - 4);
+          if (n >= 1) return "I" + Solution(n - 1);
+          throw new ArgumentOutOfRangeException("something bad happened");
+      }
+  }
+#endregion
