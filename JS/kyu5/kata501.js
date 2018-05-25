f939a01539a8ca6318545b648c0c4206f992661e
Den <denizdeniz90@gmail.com>
2018-05-25 15:51:33 +0300

    add 8th 5kuy JS kata 'Beeramid' (93rd in total)

--- JS/kyu5/kata501.js
+++ JS/kyu5/kata501.js
@@ -426,3 +426,55 @@ Description
         : str.slice(0,24) + '-\n' + wordWrap(str.slice(24));
     };
 //#endregion
+
+//#region 5008 Beeramid
+/* 5008 Beeramid (https://www.codewars.com/kata/beeramid)
+Description
+  Let's pretend your company just hired your friend from college and paid you a referral bonus. Awesome! To celebrate, you're taking your team out to the terrible dive bar next door and using the referral bonus to buy, and build, the largest three-dimensional beer can pyramid you can. And then probably drink those beers, because let's pretend it's Friday too.
+  A beer can pyramid will square the number of cans in each level - 1 can in the top level, 4 in the second, 9 in the next, 16, 25...
+  Complete the beeramid function to return the number of complete levels of a beer can pyramid you can make, given the parameters of:
+  1) your referral bonus, and
+  2) the price of a beer can
+
+  For example:
+    beeramid(1500, 2); // should === 12
+    beeramid(5000, 3); // should === 16
+*/
+
+//My solution
+    // Returns number of complete beeramid levels
+    let beeramid = function(bonus, price) {
+      if(bonus < price) return 0;
+      let cans = Math.floor(bonus / price);
+      let level = 0;
+      let count = 0;
+      while(count <= cans) {
+          level++;
+          count +=level*level;
+      }
+      return level-1;
+    }
+
+
+//Solution(s) I like(links):
+//1) Best(4) and Clever(3) https://www.codewars.com/kata/reviews/51e04f6b544cf3f6550000c4/groups/5446b794990c92b90d00130f
+    var beeramid = function(bonus, price) {
+      var beers = Math.floor(bonus / price), levels = 0;
+      while(beers >= ++levels * levels) {
+        beers -= levels * levels;
+      }
+      return levels - 1;
+    }
+//2) Clever(1) https://www.codewars.com/kata/reviews/51e04f6b544cf3f6550000c4/groups/57f49e9081f9f23759000091
+    // Returns number of complete beeramid levels
+    var beeramid = function(bonus, price) {
+      for (var i = 1; ; i++) {
+        if (level(i) > Math.floor(bonus / price)) return i - 1;
+      }
+    }
+
+    var level = function(cur) {
+      if (cur > 1)  return cur * cur + level(cur - 1);
+      else return 1;
+    }
+//#endregion
