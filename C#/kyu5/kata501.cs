60af3bf99d20904e58ee6a5054062ad69af5a5fe
Den <denizdeniz90@gmail.com>
2018-06-04 13:02:49 +0300

    add 3rd 5kuy JS kata 'Mean Square Error' (100th in total)

--- C#/kyu5/kata501.cs
+++ C#/kyu5/kata501.cs
@@ -227,3 +227,45 @@ Description:
     }
 
 #endregion
+
+
+#region 5003 Mean Square Error
+/* 5003 Mean Square Error (https://www.codewars.com/kata)
+Description
+  Complete the function that
+
+  accepts two arrays of equal length
+  compares the value each member in one array to the corresponding member in the other
+  squares the absolute value difference between those two values
+  and returns the average of those squared absolute value difference between each member pair.
+      solution([1,2,3], [4,5,6]) // should === 9 ((9 + 9 + 9) / 3)
+      solution([10, 20, 10, 2], [10, 25, 5, -2]) // should === 16.5 ((0 + 25 + 25 + 16) / 4)
+      solution([-1,0], [0,-1]) // should === 1 ((1 + 1) / 2)
+*/
+
+//My solution
+    using System;
+
+    public class Kata
+    {
+        public static double Solution(int[] firstArray, int[] secondArray)
+        {
+            var sum = 0.00;
+
+            for (var i = 0; i < firstArray.Length; i++)
+            {
+                sum += Math.Pow(secondArray[i] - firstArray[i], 2);
+            }
+            return sum / firstArray.Length;
+        }
+    }
+//Solution(s) I like(links):
+//1)  Best(4) https://www.codewars.com/kata/reviews/59961be68341a93ac7002103/groups/59be76cbf220c3ae9f0012bd
+    using System.Linq;
+    public class Kata
+    {
+      public static double Solution(int[] xs, int[] ys) =>
+        xs.Zip(ys, (x,y) => (x-y)*(x-y)).Average();
+    }
+//1)  Best(2)
+#endregion
