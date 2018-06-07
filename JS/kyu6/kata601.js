a63e65b4c680df39b1d9cd2d0434c6d917dc3c8b
Den <denizdeniz90@gmail.com>
2018-06-07 12:53:49 +0300

    add 15th 6kuy JS kata 'Imperfect Network #1 (duplicate messages)' (103rd in total)

--- JS/kyu6/kata601.js
+++ JS/kyu6/kata601.js
@@ -691,3 +691,150 @@ Description:
       return JSON.parse(JSON.stringify(a).replace(/(["\w]+)(,\1)*/g, '$1'));
     }
 //#endregion
+
+//#region 6015 Imperfect Network #1 (duplicate messages)
+/* 6015 Imperfect Network #1 (duplicate messages) (https://www.codewars.com/kata/imperfect-network-number-1-duplicate-messages)
+Description:
+  You are given the outline of a network client. The network client as it is assumes that the network will never corrupt any data, will never lose any data, will always deliver data in the order it was sent, and will never duplicate any data.
+
+  For the purposes of this Kata, assume that the network will never corrupt any data, will never lose any data, will always deliver data in the order it was sent, but that it may send duplicate data. Ensure that the callback function is not invoked for these duplicates.
+
+  The data sent into the network client's send() method will always be a string. The data sent into the sendFunction must also be a string. That data will be received by the peer one or more times in this Kata.
+
+  I will create two instances of your class: A and B. I will use A to send data to B and B to send data to A. Data sent from A to B may arrive at B more than once. Data sent from B to A may arrive more than once. If the test case says: "B sends three duplicates to A" that means that A sent some data to B and B received it three times.
+
+  Here is an example of a perfect network that would use your network class:
+
+    function PerfectNetwork(callbackA, callbackB) {
+        var network = this;
+        this.clientA = new NetworkClient(
+            function (data) { network.clientB.recv(data); }, callbackA);
+        this.clientB = new NetworkClient(
+            function (data) { network.clientA.recv(data); }, callbackB);
+    };
+
+    var perfect = new PerfectNetwork(
+        function (data) { console.log("CLIENT-A Got: " + data); },
+        function (data) { console.log("CLIENT-B Got: " + data); }
+    );
+
+    perfect.clientA.send("abcd");
+    perfect.clientA.send("wxyz");
+    perfect.clientB.send("1234");
+    perfect.clientA.send("EOF");
+    This would output:
+
+    CLIENT-B Got: abcd
+    CLIENT-B Got: wxyz
+    CLIENT-A Got: 1234
+    CLIENT-B Got: EOF
+    In this Kata, the naive client outline could very well produce the following output with a less-than-perfect network for that same sequence of send calls:
+
+    CLIENT-B Got: abcd
+    CLIENT-B Got: abcd
+    CLIENT-B Got: wxyz
+    CLIENT-A Got: 1234
+    CLIENT-A Got: 1234
+    CLIENT-B Got: EOF
+    CLIENT-B Got: EOF
+    CLIENT-B Got: EOF
+*/
+
+//My solution
+    function NetworkClient (sendFunction, callback) {
+      this.sendFunction = sendFunction;
+      this.callback = callback;
+      this.id = 0;
+    }
+
+    NetworkClient.prototype.send = function (data) {
+      // Could wrap data with extra information to send
+      this.sendFunction(JSON.stringify({data:data, id:++this.id}));
+    };
+
+    NetworkClient.prototype.recv = function (data) {
+      // Could unpack data and validate
+      let pack = JSON.parse(data);
+      if(this.id !== pack.id){
+        this.callback(pack.data);
+        this.id=pack.id;
+      }
+    };
+
+
+//Solution(s) I like(links):
+//1) Best(8) and Clever(2) https://www.codewars.com/kata/reviews/52092a7cea2911840500002a/groups/54b42c7114d9229de1000dea
+    function NetworkClient (sendFunction, callback) {
+      this.sendFunction = sendFunction;
+      this.callback = callback;
+      this.msgNum = 0;
+    }
+    NetworkClient.prototype.send = function (data) {
+      var packet = JSON.stringify({data:data, msgNum:++this.msgNum});
+      this.sendFunction(packet);
+    };
+    NetworkClient.prototype.recv = function (packet) {
+      packet = JSON.parse(packet);
+      if (packet.msgNum != this.msgNum) {
+        this.callback(packet.data);
+        this.msgNum = packet.msgNum;
+      }
+    };
+//2) Clever(4) https://www.codewars.com/kata/reviews/52092a7cea2911840500002a/groups/5620e1cdb3c7d7aee4000056
+    function NetworkClient (sendFunction, callback) {
+      this.sendFunction = sendFunction;
+      this.callback = callback;
+    }
+
+    NetworkClient.prototype.send = function (data) {
+      NetworkClient.prototype.sended = true;
+      this.sendFunction(data);
+    };
+
+    NetworkClient.prototype.recv = function (data) {
+      if (NetworkClient.prototype.sended) {
+          NetworkClient.prototype.sended = false;
+          this.callback(data);
+      }
+    };
+//3) Best(4) https://www.codewars.com/kata/reviews/52092a7cea2911840500002a/groups/54d0a4af8a433d53da00000b
+    function NetworkClient (sendFunction, callback) {
+      this.sendFunction = sendFunction;
+      this.callback = callback;
+      this.sequence = 0;
+      this.receiveSequence = -1;
+    }
+
+    NetworkClient.prototype.send = function (data) {
+      this.sendFunction(JSON.stringify({payload: data, sequence: this.sequence++}));
+    };
+
+    NetworkClient.prototype.recv = function (data) {
+      data = JSON.parse(data);
+      if (data.sequence > this.receiveSequence) {
+        this.receiveSequence = data.sequence;
+        this.callback(data.payload);
+      }
+    };
+//4) Clever(4) https://www.codewars.com/kata/reviews/52092a7cea2911840500002a/groups/5404c85929d3ef7a2b000cec
+    function NetworkClient (sendFunction, callback) {
+      this.sendFunction = sendFunction;
+      this.callback = callback;
+      this.counter = 0;
+    }
+
+    NetworkClient.prototype.send = function (data) {
+      this.counter++;
+      this.sendFunction(JSON.stringify({
+        content: data,
+        timestamp: Date.now() + this.counter
+      }));
+    };
+
+    NetworkClient.prototype.recv = function (data) {
+      data = JSON.parse(data);
+      if(this.last && this.last === data.timestamp) return;
+      this.last = data.timestamp;
+      this.callback(data.content);
+    };
+//#endregion
