159e4b16f257153e742dcd3d8b8f1f1df51688f9
Den <denizdeniz90@gmail.com>
2018-06-12 12:54:37 +0300

    add 19th 8kuy JS kata kata 'Welcome to the City' (106th in total)

--- JS/kyu8/kata801.js
+++ JS/kyu8/kata801.js
@@ -622,3 +622,28 @@ Description:
       return !!--n;
     }
 //#endregion
+
+//#region 8019 Welcome to the City
+/* 8019 Welcome to the City (https://www.codewars.com/kata/welcome-to-the-city/javascript)
+Description:
+  Create a method sayHello/say_hello/SayHello that takes as input a name, city, and state to welcome a person. Note that name will be an array consisting of one or more values that should be joined together with one space betweeen each, and the length of the name array in test cases will vary.
+  Example:
+
+    sayHello(['John', 'Smith'], 'Phoenix', 'Arizona')
+  This example will return the string Hello, John Smith! Welcome to Phoenix, Arizona!
+*/
+
+//My solution
+    function sayHello( name, city, state ) {
+      return `Hello, ${name.join(' ')}! Welcome to ${city}, ${state}!`;
+    }
+
+//Solutions I like:
+//1) Best(6) https://www.codewars.com/kata/reviews/559375e5091cf8e47d00005a/groups/55a8fc5f5ee7728cdf000010
+    var sayHello = (n, c, s) => `Hello, ${n.join(' ')}! Welcome to ${c}, ${s}!`;
+//2)Clever(8) https://www.codewars.com/kata/reviews/559375e5091cf8e47d00005a/groups/559479f2ada4e84afd0000f4
+    function sayHello( a, b, c) {
+      return 'Hello, ' + a.join(' ') + '! Welcome to ' + [b,c].join(', ') + '!';
+    }
+
+//#endregion
