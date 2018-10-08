a28236ac5ee5520ddb0ff6ee760609c3877d6fc2
Den <denizdeniz90@gmail.com>
2018-10-08 16:34:54 +0300

    add 23rd 6kuy JS kata 'Custom Setters and Getters' (149th in total)

--- JS/kyu6/kata601.js
+++ JS/kyu6/kata601.js
@@ -1241,3 +1241,121 @@ Description:
     }
 
 //#endregion
+
+//#region 6023 Custom Setters and Getters
+/* 6023 Custom Setters and Getters (https://www.codewars.com/kata/custom-setters-and-getters)
+Description:
+  Let's take a look at an interesting feature of JS: custom Getters and Setters for objects.
+  Skimming over this may help you prepare for this kata: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects?redirectlocale=en-US&redirectslug=JavaScript%2FGuide%2FWorking_with_Objects#Defining_Getters_and_Setters (thanks to @contablebrew for pointing out the more "forward-compatible" approach to this.)
+  The object of this exercise will be to provide a constructor named "Archiver" that creates a "self-archiving" object with respect to its "temperature" property, but otherwise looks and acts just like a normal JS object. Each time the "temperature" property is assigned, the object should add a log entry to an array (the "archive") that it returns via a "getArchive" function. The log entry should be formed as such: {date: //the Date when the "temperature" property was assigned, val: //the value that was written to the "temperature" property.} . The "date" property of the log entry should be a valid JS Date object.
+  so e.g.:
+
+    //new Date() == '2013-09-24...Z'
+    var arc = new Archiver();
+    arc.temperature = 33;
+    arc.temperature = 28;
+    arc.temperature = 21;
+    arc.getArchive() // == [{date: 2013-09-24..., val:33},{date: 2013-09-24..., val:28},{date: 2013-09-24..., val:21}]
+*/
+
+//My solution
+    //TODO: implement "Archiver" as a self-archiving object
+    //...ensure that when "temperature" property is
+    //...set, that the array returned by getArchive
+    //...gets a log entry in the form of {date:,val:}
+    class Archiver {
+      constructor() {
+        this.archive = [];
+        this.temp = null;
+      }
+
+      set temperature(t) {
+        this.temp = t;
+        this.archive.push({date: new Date(), val: t});
+      }
+
+      get temperature() {
+        return this.temp;
+      }
+
+      getArchive() {
+        return this.archive;
+      }
+    }
+
+
+
+
+//Solution(s) I like(links):
+//1) Best(25) !!Comment https://www.codewars.com/kata/reviews/5241060ff2471a5d7600025d/groups/5243809095c2a6c74900036e
+    function Archiver() {
+      var temperature = null;
+      var archive = [];
+      Object.defineProperty(this, 'temperature', {
+        get: function() { return temperature; },
+        set: function(x) {
+          temperature = x;
+          archive.push({
+            date: new Date(),
+            val: x
+          });
+        }
+      });
+
+      this.getArchive = function() {return archive;};
+    }
+//2) Best(7) Comment https://www.codewars.com/kata/reviews/5241060ff2471a5d7600025d/groups/537e5ee2066950ea4e000044
+    function Archiver() {
+      var temperature = null;
+      var archive = [];
+
+      this.__defineSetter__("temperature", function(val){
+        archive.push({date: new Date(), val: val});
+        temperature = val;
+      });
+      this.__defineGetter__("temperature", function(val){
+        return temperature;
+      });
+
+      this.getArchive = function() {return archive;};
+    }
+//3) Best(6) https://www.codewars.com/kata/reviews/5241060ff2471a5d7600025d/groups/5398e325c729da14560014b9
+    function Archiver() {
+      var archive = [];
+      this.getArchive = function() {
+        return archive;
+      };
+    }
+
+    Archiver.prototype = {
+      set temperature(v) {
+        this.temp = v;
+        this.getArchive().push({ date: new Date(), val: v });
+      },
+      get temperature() {
+        return this.temp;
+      }
+    };
+//4) Clever(2) https://www.codewars.com/kata/reviews/5241060ff2471a5d7600025d/groups/53d9a1984a5168bd65000088
+    function Archiver() {
+      this.temp = null;
+      this.archive = [];
+      //TODO: implement "Archiver" as a self-archiving object
+      //...ensure that when "temperature" property is
+      //...set, that the array returned by getArchive
+      //...gets a log entry in the form of {date:,val:}
+      this.getArchive = function() {return this.archive;};
+      this.setArchive = function(d,t) {
+        this.archive.push({date: d, val:t});
+      }
+    }
+    Object.defineProperty(Archiver.prototype, "temperature", {
+    get: function() { return this.temp },
+    set: function(t) {
+      var d = new Date();
+      this.temp = t;
+      this.setArchive(d,t);
+    }
+    });
+
+//#endregion
