0d78c56ff8ec15e549ad63ca651c39c1e7edb544
Den <denizdeniz90@gmail.com>
2018-11-19 12:58:41 +0300

    add 27th 8kuy JS kata 'Reverse List Order' (169th in total)

--- JS/kyu8/kata801.js
+++ JS/kyu8/kata801.js
@@ -894,3 +894,30 @@ function even_or_odd(number) {
     }
 
 //#endregion
+
+
+//#region 8027 Reverse List Order
+/*8027 Reverse List Order (https://www.codewars.com/kata/reverse-list-order)
+Description:
+  In this kata you will create a function that takes in a list and returns a list with the reverse order.
+  Examples
+    reverseList([1,2,3,4]) == [4,3,2,1]
+    reverseList([3,1,5,4]) == [4,5,1,3]
+*/
+
+//My solution
+    function reverseList(list) {
+      return list.reverse();
+    }
+  //const reverseList = list => list.reverse();
+
+//Solutions I like:
+//1) Best(4) Comment https://www.codewars.com/kata/reviews/5991b276c015783355000014/groups/5991eb5c2e9e59c45400023d
+    function reverseList(list) {
+      return list.slice().reverse();
+    }
+//2) Clever(2) https://www.codewars.com/kata/reviews/5991b276c015783355000014/groups/5aad3015631725cc3f000146
+    function reverseList(list) {
+      return list.reduce((reverseList, element) => [element, ...reverseList], []);
+    }
+//#endregion
