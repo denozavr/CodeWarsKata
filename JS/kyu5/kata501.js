86e788fcf1499f036a6735e7a447fff9b16e2ef5
Den <denizdeniz90@gmail.com>
2019-03-05 13:08:48 +0300

    add 20th 5kuy JS kata 'Game - Electrons around the cores' (186th in total)

--- JS/kyu5/kata501.js
+++ JS/kyu5/kata501.js
@@ -1214,3 +1214,66 @@ Description
     }
 
 //#endregion
+
+
+
+//#region 5020 Game - Electrons around the cores
+/* 5020 Game - Electrons around the cores (https://www.codewars.com/kata/game-electrons-around-the-cores/)
+Description
+  Have you heard of the game "electrons around the cores"? I'm not allowed to give you the complete rules of the game, just so much:
+
+  the game is played with 4 to 6 dice, so you get an array of 4 to 6 numbers, each 1-6
+  the name of the game is important
+  you have to return the correct number five times in a row and your solution is considered to be correct
+  If you just press "submit", you'll get an array and the expected value!
+
+  Here are some input/output pairs for you to wrap your mind around:
+
+    [ 1, 2, 3, 4, 5 ] -> 6
+    [ 2, 2, 3, 3 ] -> 4
+    [ 6, 6, 4, 4, 1, 3 ] -> 2
+    [ 3, 5, 3, 5, 4, 2 ] -> 12
+  And yes, it is a shameless copy of a famous game.
+*/
+
+//My solution
+function electrons_around_the_cores( dice ){
+  // Just so you can try some numbers
+  return  dice.count(5) * 4 + dice.count(3) * 2;
+}
+
+Array.prototype.count = function(num) {
+ let result = 0;
+ for(let el in this) {
+  if(this[el] === num) {
+     result++;
+   }
+  }
+  return result;
+};
+
+//Solution(s) I like(links):
+//1) Best(3) https://www.codewars.com/kata/reviews/522107aab209efd4b6000112/groups/5417361e5c5c64ef910001ac
+    function electrons_around_the_cores( dice ){
+      var sum = 0;
+      dice.forEach(function(value) {
+        if (value%2 === 1) {
+          sum += value - 1;
+        }
+      });
+
+      return sum;
+    }
+//2) Best(3) https://www.codewars.com/kata/reviews/522107aab209efd4b6000112/groups/54f11f234507b26e5f000c43
+    function electrons_around_the_cores( dice ){
+      return dice.reduce(function(prevTotal, currentDie){
+        //each die's score is how many dots are around the central dot
+        currentDie = (currentDie == 3 || currentDie == 5) ? currentDie - 1 : 0;
+        return prevTotal + currentDie;
+      }, 0);
+    }
+//3) Clever(4) https://www.codewars.com/kata/reviews/522107aab209efd4b6000112/groups/53f601c4c0c45d986900c660
+    function electrons_around_the_cores( dice ){
+      return dice.reduce(function(s,d){ return s+d%2*(d-1) },0)
+    }
+//#endregion
