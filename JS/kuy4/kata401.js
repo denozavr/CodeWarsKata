c9b7eeff6d4609c0739791a0957fbaba6658b26f
Den <denizdeniz90@gmail.com>
2019-03-12 17:59:57 +0300

    add 12th 4kuy JS kata 'Snail' (190th in total)

--- JS/kuy4/kata401.js
+++ JS/kuy4/kata401.js
@@ -1109,3 +1109,94 @@ Description:
       }
     }
 //#endregion
+
+//#region 4012 Snail
+/* 4012 Snail (https://www.codewars.com/kata/snail/javascript)
+Description:
+  Snail Sort
+  Given an n x n array, return the array elements arranged from outermost elements to the middle element, traveling clockwise.
+
+    array = [[1,2,3],
+            [4,5,6],
+            [7,8,9]]
+    snail(array) #=> [1,2,3,6,9,8,7,4,5]
+  For better understanding, please follow the numbers of the next array consecutively:
+
+    array = [[1,2,3],
+            [8,9,4],
+            [7,6,5]]
+    snail(array) #=> [1,2,3,4,5,6,7,8,9]
+
+  NOTE: The idea is not sort the elements from the lowest value to the highest; the idea is to traverse the 2-d array in a clockwise snailshell pattern.
+
+  NOTE 2: The 0x0 (empty matrix) is represented as [[]]
+*/
+
+//My solution
+    function snail(array) {
+      let results = [];
+
+      while(array.length) {
+        results = results.concat(array.shift());
+
+        array.forEach(function (current) {
+          results.push(current.pop());
+        });
+
+        array.forEach(function (current) {
+          current.reverse();
+        });
+
+        array.reverse();
+      }
+
+      return results;
+    }
+
+//Solution(s) I like(links):
+//1) Clever(141) !!Comment https://www.codewars.com/kata/reviews/5220c5750b1ba099360003e2/groups/55213b8f48b3baa1b90009ff
+    snail = function(array) {
+      var result;
+      while (array.length) {
+        // Steal the first row.
+        result = (result ? result.concat(array.shift()) : array.shift());
+        // Steal the right items.
+        for (var i = 0; i < array.length; i++)
+          result.push(array[i].pop());
+        // Steal the bottom row.
+        result = result.concat((array.pop() || []).reverse());
+        // Steal the left items.
+        for (var i = array.length - 1; i >= 0; i--)
+          result.push(array[i].shift());
+      }
+      return result;
+    }
+//2) Clever(58) COmment https://www.codewars.com/kata/reviews/5220c5750b1ba099360003e2/groups/583b55348bbc048410000c2c
+    function snail(array) {
+      var vector = [];
+      while (array.length) {
+        vector.push(...array.shift());
+        array.map(row => vector.push(row.pop()));
+        array.reverse().map(row => row.reverse());
+      }
+      return vector;
+    }
+//3) Best(5) https://www.codewars.com/kata/reviews/5220c5750b1ba099360003e2/groups/57442b74ce32aa4ae4000835
+    snail = function(array) {
+      var size = array.length;
+
+      if (size == 0)
+        return [];
+
+      if (size == 1)
+        return array[0];
+
+      var top    = array[0].slice(0, -1);
+      var right  = array.slice(0, -1).map(a => a[size - 1]);
+      var bottom = array[size -1].slice(1).reverse();
+      var left   = array.slice(1).map(a => a[0]).reverse();
+      var inner  = array.slice(1, -1).map(a => a.slice(1, -1));
+
+      return [].concat(top, right, bottom, left, snail(inner));
+    }
+//#endregion
